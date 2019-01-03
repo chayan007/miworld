877d30a84cda5c658ff3c19f00f8e4dfb11f25bc
Chayan Datta <sonicxxx7@gmail.com>
2019-01-03 12:04:59 +0530

    User model created

--- miworld/settings.py
+++ miworld/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'users',
 ]
 
+AUTH_USER_MODEL = 'core.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from django.utils.text import slugify
 
 # Create your models here.
+
+class User(AbstractUser):
+    gender = models.CharField(choices=(
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Others'),
+    ))
+    slug = models.SlugField(unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    is_celeb = models.BooleanField(default=False)
+    is_premium = models.BooleanField(default=False)
+    verification_id = models.FileField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.first_name + str(self.birth_date))
+        super(User, self).save(*args, **kwargs)
