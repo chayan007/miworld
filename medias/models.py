dc922810207282b13d44b79d0ce38e905ce39a35
Chayan Datta <sonicxxx7@gmail.com>
2019-01-05 13:39:34 +0530

    Media Models added

--- medias/models.py
+++ medias/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/')
+    extension = models.CharField(max_length=5)
+    memory = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.File(upload_to='videos/')
+    extension = models.CharField(max_length=5)
+    memory = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Audio(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.FileField(upload_to='images/')
+    extension = models.CharField(max_length=5)
+    memory = models.IntegerField()
+
+    def __str__(self):
+        return self.name
