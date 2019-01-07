2d13dc35452b2df69027016327ba954d2cd705b1
Chayan Datta <sonicxxx7@gmail.com>
2019-01-07 14:00:32 +0530

    Like and Comment router complete

--- miworld/post_router.py
+++ miworld/post_router.py
@@ -1,5 +1,10 @@
 from posts.post_api.viewsets import PostViewSet
+from posts.like_api.viewsets import LikeViewSet
+from posts.comment_api.viewsets import CommentViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+
 router.register('', PostViewSet)
+router.register('likes', LikeViewSet)
+router.register('comments', CommentViewSet)
