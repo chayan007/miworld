44b9badd590f04648fb0cb125c079f5457d88fc4
Chayan Datta <sonicxxx7@gmail.com>
2019-01-11 12:00:16 +0530

    Settings configured

---
+++ posts/post_api/generics.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from rest_framework import mixins
+from posts.post_api.serializers import PostSerializer
+from posts.models import Post
+
+
+class PostView(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
