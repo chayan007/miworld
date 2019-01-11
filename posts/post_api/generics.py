c7fe94e458f3131244d87a772b2060b310dad511
Chayan Datta <sonicxxx7@gmail.com>
2019-01-11 13:04:54 +0530

    Notifications added

--- posts/post_api/generics.py
+++ posts/post_api/generics.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.response import Response
 from posts.post_api.serializers import PostSerializer
 from posts.models import Post
+from django.contrib.auth.models import User
+from notifications.models import Notification
 
 
-class PostView(generics.GenericAPIView, mixins.ListModelMixin,
-                                        mixins.RetrieveModelMixin):
+class PostView(generics.GenericAPIView,
+               mixins.ListModelMixin,
+               mixins.RetrieveModelMixin,
+               mixins.CreateModelMixin,
+               ):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     lookup_field = 'id'
@@ -16,3 +22,8 @@ class PostView(generics.GenericAPIView, mixins.ListModelMixin,
         else:
             return self.list(request)
 
+    def post(self, request):
+        self.create(request)
+        notification = Notification(user= User.objects.get(id = request.POST['user']), notification= 'added a post', link= '')
+        notification.save()
+        return Response({'status' : 'true'})
