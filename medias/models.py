1b1e2a4c01af297d160bed87dd8b154f11142924
Chayan Datta <sonicxxx7@gmail.com>
2019-01-16 15:29:14 +0530

    Cover non nullable to nullable

--- medias/models.py
+++ medias/models.py
@@ -6,7 +6,7 @@ from posts.models import Post
 class Image(models.Model):
     name = models.CharField(max_length=30 ,blank= True)
     image = models.ImageField(upload_to='images/')
-    extension = models.CharField(max_length=5,blank= True)
+    extension = models.CharField(max_length=5, blank= True)
     memory = models.IntegerField(blank= True)
     post = models.ForeignKey(Post, to_field='id', on_delete=models.CASCADE, null=True)
 
@@ -16,10 +16,10 @@ class Image(models.Model):
 class Video(models.Model):
     name = models.CharField(max_length=30, blank= True)
     video = models.FileField()
-    snap1 = models.ImageField(upload_to='video_snaps/')
-    snap2 = models.ImageField(upload_to='video_snaps/')
-    snap3 = models.ImageField(upload_to='video_snaps/')
-    cover = models.ImageField(upload_to='video_snaps/')
+    snap1 = models.ImageField(upload_to='video_snaps/', null= True)
+    snap2 = models.ImageField(upload_to='video_snaps/', null= True)
+    snap3 = models.ImageField(upload_to='video_snaps/', null= True)
+    cover = models.ImageField(upload_to='video_snaps/', null= True)
     extension = models.CharField(max_length=5)
     memory = models.IntegerField()
     post = models.ForeignKey(Post, to_field='id', on_delete=models.CASCADE, null=True)
