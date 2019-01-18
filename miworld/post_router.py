c1702c1b1f973ee025c9eb59a0e2826e5276e471
Chayan Datta <sonicxxx7@gmail.com>
2019-01-18 12:20:53 +0530

    Router created for actual posts

--- miworld/post_router.py
+++ miworld/post_router.py
@@ -1,5 +1,6 @@
 from posts.post_api.viewsets import PostViewSet
 from posts.like_api.viewsets import LikeViewSet
+from timelines.api.viewsets import ActualPostViewSet
 from posts.comment_api.viewsets import CommentViewSet
 from rest_framework import routers
 from django.urls import path
@@ -9,6 +10,7 @@ post_router = routers.DefaultRouter()
 post_router.register('posts', PostViewSet)
 post_router.register('likes', LikeViewSet)
 post_router.register('comments', CommentViewSet)
+post_router.register('actual_posts', ActualPostViewSet)
 
 # urlpatterns = [
 #     path(),
