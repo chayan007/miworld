3e328b6fc4a0fe4d4d12575076e090880d7c6da8
Chayan Datta <sonicxxx7@gmail.com>
2019-01-18 12:15:50 +0530

    Actual Post model created for Timeline

--- timelines/models.py
+++ timelines/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from posts.models import Post
+from medias.models import Image, Video
 
 # Create your models here.
+
+class Actual_Post(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post.description[:20]
+
+    def get_images(self):
+        images = Image.objects.get(post=self.post)
+        return images
