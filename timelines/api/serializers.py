179acea09ae7609a54b495b12bf66f181bdff2cb
Chayan Datta <sonicxxx7@gmail.com>
2019-01-21 13:18:27 +0530

    Actual Post serializer modified

--- timelines/api/serializers.py
+++ timelines/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from timelines.models import Actual_Post
+from posts.like_api.serializers import LikeSerializer
+from posts.comment_api.serializers import CommentSerializer
 
 
 class ActualPostSerializer(serializers.ModelSerializer):
+    likes = LikeSerializer
+    comments = CommentSerializer
 
     class Meta:
         model = Actual_Post
