2490553d1f3c14a1e52165ec218b48264db3f9a9
Chayan Datta <sonicxxx7@gmail.com>
2019-01-28 22:19:15 +0530

    Error debug #1

--- timelines/models.py
+++ timelines/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from posts.models import Post, Like, Comment
-from medias.models import Image, Video
+from medias.models import Image
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
 
+
 class Actual_Post(models.Model):
     post = models.OneToOneField(Post, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE, null=True)
     likes = models.ForeignKey(Like, on_delete=models.CASCADE, null=True)
     comments = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
-
 
     def __str__(self):
         return self.post.description[:20]
@@ -21,21 +20,20 @@ class Actual_Post(models.Model):
         images = Image.objects.get(post=self.post)
         return images
 
+
 def create_acutal_post(sender, instance, created, **kwargs):
     if created:
         Actual_Post.objects.get_or_create(post=instance)
+
 
 def add_likes(sender, **kwargs):
     actual_post = Actual_Post.objects.get(post=kwargs['post'])
     actual_post.likes = kwargs['instance']
 
+
 def add_comments(sender, instance, **kwargs):
     actual_post = Actual_Post.objects.get(post=instance.post)
     actual_post.comments = kwargs['instance']
-    
-
 
 
 post_save.connect(create_acutal_post, sender=Post)
-# post_save.connect(add_likes, sender=Like)
-# post_save.connect(add_comments, sender=Comment)
