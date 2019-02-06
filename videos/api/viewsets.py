eb1fdfbe303774081cc2e6ad9b2d424296d4ce0e
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 15:18:45 +0530

    Permission class added to Videos API

--- videos/api/viewsets.py
+++ videos/api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from .serializers import VideoSerializer, Video
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class VideoViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
