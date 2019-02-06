d10f274a0332227b4b8c488ecced7af291de3c5b
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 14:24:55 +0530

    Permission class added to Images API

--- medias/image_api/viewsets.py
+++ medias/image_api/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .serializers import ImageSerializer, Image
+from rest_framework.permissions import IsAuthenticated
 
 
 class ImageViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
