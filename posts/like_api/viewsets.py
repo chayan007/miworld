bbbedadd0c0ddddbb2655bf3e145b1ec7fdd6b42
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 15:01:35 +0530

    Permission class added to Liker API

--- posts/like_api/viewsets.py
+++ posts/like_api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from .serializers import *
 from posts.models import Like
+from rest_framework.permissions import IsAuthenticated
+
 
 class LikeViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
