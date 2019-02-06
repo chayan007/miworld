5e494648371691d70a6a6cc09c86e278913040db
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 15:00:39 +0530

    Permission class added to Comment API

--- posts/comment_api/viewsets.py
+++ posts/comment_api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .serializers import *
 from posts.models import Comment
+from rest_framework.permissions import IsAuthenticated
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
