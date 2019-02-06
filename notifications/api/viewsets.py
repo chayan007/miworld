5c3ac769af5be665c2cfce1e2aa2682bb3226904
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 14:26:58 +0530

    Permission class added to Followers API

--- notifications/api/viewsets.py
+++ notifications/api/viewsets.py
@@ -1,8 +1,10 @@
 from .serializers import NotificationSerializer
 from notifications.models import Notification
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 class NotificationViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
