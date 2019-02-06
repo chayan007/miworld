99c51d026858cca48428382e0669575ddd9bf25e
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 15:18:14 +0530

    Permission class added to Timeline API

--- timelines/api/viewsets.py
+++ timelines/api/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .serializers import ActualPostSerializer, Actual_Post, Post
+from rest_framework.permissions import IsAuthenticated
 
 
 class ActualPostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     queryset = Actual_Post.objects.all()
     serializer_class = ActualPostSerializer
