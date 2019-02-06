46714f17d1db19c56c2300794eb2674137000e98
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 14:26:11 +0530

    Permission class added to Followers API

--- followers/api/viewsets.py
+++ followers/api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from .serializers import FollowerSerializer
 from followers.models import Follower
+from rest_framework.permissions import IsAuthenticated
+
 
 class FollowerViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
