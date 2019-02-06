759edf6a9eef6c8bbd4584c36fb8a5790929804d
Chayan Datta <sonicxxx7@gmail.com>
2019-02-06 14:59:55 +0530

    Permission class added to Comment API

--- posts/comment_api/generics.py
+++ posts/comment_api/generics.py
@@ -4,37 +4,11 @@ from .serializers import CommentSerializer, Comment
 from django.contrib.auth.models import User
 from notifications.models import Notification
 from posts.models import Post
+from rest_framework.permissions import IsAuthenticated
 
 
-# class CommentView(generics.GenericAPIView,
-#                   mixins.RetrieveModelMixin,
-#                   mixins.UpdateModelMixin,
-#                   mixins.DestroyModelMixin):
-#     serializer_class = CommentSerializer
-#     queryset = Comment.objects.all()
-#
-#     def get(self, request, id=None):
-#         if id:
-#             post = Post.objects.get(id=id)
-#             #return self.retrieve(request, id)
-#             comments = Comment.objects.get(post=post)
-#             return comments
-#         else:
-#             return Response({'error': 'No specific post is selected'})
-#
-#     def patch(self, request, id):
-#         self.partial_update(request, id)
-#         notification = Notification(user=User.objects.get(id=request.POST['user']), notification= 'edited a comment')
-#         notification.save()
-#         return Response({'status': 'success'})
-#
-#     def delete(self, request, id):
-#         self.destroy(request, id)
-#         notification = Notification(user=User.objects.get(id=request.POST['user']), notification='deleted a comment')
-#         notification.save()
-#         return Response({'status': 'success'})
-#
 class CommentView(generics.GenericAPIView, mixins.RetrieveModelMixin):
+    permission_classes = (IsAuthenticated,)
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
 
