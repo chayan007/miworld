b8bc1bc87883185262378c773d383f8a7d88c407
Chayan Datta <sonicxxx7@gmail.com>
2019-02-07 10:56:07 +0530

    Token Auth class added to Images

--- medias/image_api/viewsets.py
+++ medias/image_api/viewsets.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .serializers import ImageSerializer, Image
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class ImageViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated,)
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
