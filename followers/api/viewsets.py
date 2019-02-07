b437a5b348d439d0a609dbb5694919c80ca9635a
Chayan Datta <sonicxxx7@gmail.com>
2019-02-07 10:55:25 +0530

    Token Auth class added to Followers

--- followers/api/viewsets.py
+++ followers/api/viewsets.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets
 from .serializers import FollowerSerializer
 from followers.models import Follower
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class FollowerViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
--- videos/api/viewsets.py
+++ videos/api/viewsets.py
@@ -3,7 +3,6 @@ from .serializers import VideoSerializer, Video
 from rest_framework.permissions import IsAuthenticated
 
 
-
 class VideoViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, )
     queryset = Video.objects.all()
