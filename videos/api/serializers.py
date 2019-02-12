b6e37c271066ab81c307b7b406d2ca4ec085c97a
Chayan Datta <sonicxxx7@gmail.com>
2019-02-12 12:37:42 +0530

    User serializer added in Video Serializer

--- videos/api/serializers.py
+++ videos/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from videos.models import Video
+from users.api.serializers import UserSerializer
 
 
 class VideoSerializer(serializers.ModelSerializer):
+    user = UserSerializer
+
     class Meta:
         model = Video
         fields = '__all__'
