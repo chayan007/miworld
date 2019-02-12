5166df29e5195b6f7bd6cefa13f1543f3da4ee26
Chayan Datta <sonicxxx7@gmail.com>
2019-02-12 12:12:05 +0530

    Story Model created

--- story/models.py
+++ story/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Story(models.Model):
+    story_text = models.TextField(max_length=300, null=True)
+    story_image = models.ImageField(upload_to='story/', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.first_name
