969c551acbd9b3872988756aa4bbab7f42a990cc
Chayan Datta <sonicxxx7@gmail.com>
2019-02-24 17:49:33 +0530

    User bind ID to videos API and Story API

--- story/models.py
+++ story/models.py
@@ -6,7 +6,7 @@ class Story(models.Model):
     story_text = models.TextField(max_length=300, null=True)
     story_image = models.ImageField(upload_to='story/images/', null=True)
     story_video = models.FileField(upload_to='story/videos/', null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
